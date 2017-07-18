22c4fbe017b75d3e36729886e41d44dd4291b522
janeosaur <janewie@gmail.com>
2017-07-18 15:57:29 -0700

    push to heroku

--- models/index.js
+++ models/index.js
@@ -1,2 +1,5 @@
+var mongoose = require("mongoose");
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/Wayfarer");
+
 module.exports.Comment = require('./comments');
 module.exports.City = require('./cities')
