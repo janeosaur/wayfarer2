15a2a7d153a2037bec64256ca2e8e83a759748ec
janeosaur <janewie@gmail.com>
2017-07-18 16:04:34 -0700

    changes ajax urls to heroku deployed api

--- src/UserPostBox.js
+++ src/UserPostBox.js
@@ -13,7 +13,7 @@ class UserPostBox extends Component {
   loadPostsFromServer() {
     $.ajax ({
       method: 'GET',
-      url: `http://localhost:3001/api/profile/comments/${window.Myvars.uid}`
+      url: `https://wayfarer2-api.herokuapp.com/api/profile/comments/${window.Myvars.uid}`
     })
     .then(res => {
       this.setState({data: res})
@@ -29,6 +29,7 @@ class UserPostBox extends Component {
     return (
       <div>
         <Row className="post-box">
+          <h3 className="">Your most recent posts...</h3>
           <UserPostList
             data={this.state.data}/>
         </Row>
